3cc4716216e79bd7c26f7df8e47afe80cdeed18c
nandita <e1249141@u.nus.edu>
2025-01-30 20:25:37 +0800

    store tasks in a list, and print out the list of tasks

--- src/main/java/Bork.java
+++ src/main/java/Bork.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Bork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0; // tracking number of tasks
 
         System.out.println("Hello! I'm Bork");
         System.out.println("What can I do for you?");
@@ -14,8 +16,24 @@ public class Bork {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            // echo
-            System.out.println(" " + input);
+
+            if (input.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks added yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + " . " + tasks[i]);
+                    }
+                }
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("added: " + input);
+                } else {
+                    System.out.println("Task list is full! Cannot add more.");
+                }
+            }
         }
         scanner.close();
     }
