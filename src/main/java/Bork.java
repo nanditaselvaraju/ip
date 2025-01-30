919774091ad88be1a19671aefd13791c258840d8
nandita <e1249141@u.nus.edu>
2025-01-30 16:10:00 +0800

    added code to echo command given by the user

--- src/main/java/Bork.java
+++ src/main/java/Bork.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Bork {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Bork");
         System.out.println("What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            // echo
+            System.out.println(" " + input);
+        }
+        scanner.close();
     }
 }
